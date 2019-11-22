25417c0719ba151836be5c6e2ad2d4ec0a74620e
Rui Ventura <rventura.pt@outlook.com>
2019-11-22 19:49:42 +0000

    Oops, forgot algebra.cpp

--- src/algebra.cpp
+++ src/algebra.cpp
@@ -1,33 +1,33 @@
 #include <CGAL/number_utils.h>
 
-#include <jlcxx/jlcxx.hpp>
+#include <jlcxx/module.hpp>
 
 #include "macros.hpp"
-#include "enum.hpp"
+#include "kernel.hpp"
 
-#include "algebra.hpp"
+using NT = Kernel::FT;
 
 void wrap_algebra(jlcxx::Module& cgal) {
   // missing functions that involve EuclideanRing concept:
   // div, div_mod, mod
-  GLOBAL_FUNCTION(NT, abs, const NT&);
-  GLOBAL_FUNCTION(CGAL::Comparison_result, compare, const NT&, const NT&);
-  GLOBAL_FUNCTION(NT, integral_division, const NT&, const NT&);
-  GLOBAL_FUNCTION(NT, inverse, const NT&);
-  GLOBAL_FUNCTION(bool, is_negative, const NT&);
-  GLOBAL_FUNCTION(bool, is_positive, const NT&);
-  GLOBAL_FUNCTION(bool, is_square, const NT&);
-  GLOBAL_FUNCTION(bool, is_square, const NT&, NT&);
-  GLOBAL_FUNCTION(bool, is_zero, const NT&);
-  GLOBAL_FUNCTION(CGAL::Sign, sign, const NT&);
+  CGAL_GLOBAL_FUNCTION(NT, abs, const NT&);
+  CGAL_GLOBAL_FUNCTION(CGAL::Comparison_result, compare, const NT&, const NT&);
+  CGAL_GLOBAL_FUNCTION(NT, integral_division, const NT&, const NT&);
+  CGAL_GLOBAL_FUNCTION(NT, inverse, const NT&);
+  CGAL_GLOBAL_FUNCTION(bool, is_negative, const NT&);
+  CGAL_GLOBAL_FUNCTION(bool, is_positive, const NT&);
+  CGAL_GLOBAL_FUNCTION(bool, is_square, const NT&);
+  CGAL_GLOBAL_FUNCTION(bool, is_square, const NT&, NT&);
+  CGAL_GLOBAL_FUNCTION(bool, is_zero, const NT&);
+  CGAL_GLOBAL_FUNCTION(CGAL::Sign, sign, const NT&);
   // no matching method for given NT
   //GLOBAL_FUNCTION(void, simplify, const NT&);
-  GLOBAL_FUNCTION(NT, sqrt, const NT&);
-  GLOBAL_FUNCTION(NT, square, const NT&);
-  GLOBAL_FUNCTION(double, to_double, const NT&);
+  CGAL_GLOBAL_FUNCTION(NT, sqrt, const NT&);
+  CGAL_GLOBAL_FUNCTION(NT, square, const NT&);
+  CGAL_GLOBAL_FUNCTION(double, to_double, const NT&);
   cgal.method("to_interval", [](const NT& nt) {
       auto pair = CGAL::to_interval(nt);
       return std::make_tuple(pair.first, pair.second);
   });
-  GLOBAL_FUNCTION(NT, unit_part, const NT&);
+  CGAL_GLOBAL_FUNCTION(NT, unit_part, const NT&);
 }
