9865410bcbbe1a0443632f41d33e25ca694e03ea
Rui Ventura <rventura.pt@outlook.com>
2019-12-10 00:33:24 +0000

    Restore enum.cpp, not ready yet

--- src/enum.cpp
+++ src/enum.cpp
@@ -5,41 +5,31 @@
 #include "macros.hpp"
 
 #define CGAL_CONST(N)   cgal.CONST(N, CGAL::N)
-#define CGAL_ENUM(E, N) cgal.BITS_ST(CGAL::E, N, jlcxx::julia_type("CppEnum"))
-#define CGAL_SENUM(E)   CGAL_ENUM(E, E)
 
 void wrap_enum(jlcxx::Module& cgal) {
-  CGAL_SENUM(Sign);
-  OVERRIDE_BASE(cgal,);
-  cgal.UNARY_OP(-, CGAL::Sign);
-  cgal.BINARY_OP(CGAL::Sign, *, CGAL::Sign);
-  UNSET_OVERRIDE(cgal,);
-  CGAL_SIMPLE_PFUNC(opposite, CGAL::Sign);
-  // Sign
-  CGAL_CONST(NEGATIVE ); CGAL_CONST(ZERO    ); CGAL_CONST(POSITIVE);
-  CGAL_CONST(COLLINEAR); CGAL_CONST(COPLANAR); CGAL_CONST(DEGENERATE);
-  // Orientation
-  CGAL_CONST(RIGHT_TURN); CGAL_CONST(LEFT_TURN);
-  CGAL_CONST(CLOCKWISE ); CGAL_CONST(COUNTERCLOCKWISE);
-  // Oriented_side
-  CGAL_CONST(ON_NEGATIVE_SIDE); CGAL_CONST(ON_ORIENTED_BOUNDARY); CGAL_CONST(ON_POSITIVE_SIDE);
-  // Comparison_result
-  CGAL_CONST(SMALLER); CGAL_CONST(EQUAL); CGAL_CONST(LARGER);
+  jl_value_t *jl_cpp_enum = jlcxx::julia_type("CppEnum");
 
-  CGAL_ENUM(Bounded_side, BoundedSide);
-  CGAL_UNAMBIG_FUNC(CGAL::Bounded_side, opposite, CGAL::Bounded_side);
-  CGAL_CONST(ON_UNBOUNDED_SIDE); CGAL_CONST(ON_BOUNDARY); CGAL_CONST(ON_BOUNDED_SIDE);
-
-  CGAL_SENUM(Angle);
-  CGAL_UNAMBIG_FUNC(CGAL::Angle, opposite, CGAL::Angle);
+  cgal.add_bits<CGAL::Angle>("Angle", jl_cpp_enum);
   CGAL_CONST(OBTUSE); CGAL_CONST(RIGHT); CGAL_CONST(ACUTE);
 
-  CGAL_ENUM(Box_parameter_space_2, BoxParameterSpace2);
-  CGAL_CONST(LEFT_BOUNDARY  ); CGAL_CONST(RIGHT_BOUNDARY);
-  CGAL_CONST(BOTTOM_BOUNDARY); CGAL_CONST(TOP_BOUNDARY  );
-  CGAL_CONST(INTERIOR       ); CGAL_CONST(EXTERIOR      );
+  cgal.add_bits<CGAL::Bounded_side>("BoundedSide", jl_cpp_enum);
+  CGAL_CONST(ON_UNBOUNDED_SIDE); CGAL_CONST(ON_BOUNDARY); CGAL_CONST(ON_BOUNDED_SIDE);
+
+  // Box_parameter_space_2 is causing issues, skipping it
+
+  cgal.add_bits<CGAL::Sign>("Sign", jl_cpp_enum);
+  CGAL_CONST(NEGATIVE); CGAL_CONST(ZERO); CGAL_CONST(POSITIVE);
+
+  // Orientation
+  CGAL_CONST(LEFT_TURN); CGAL_CONST(RIGHT_TURN);
+  CGAL_CONST(CLOCKWISE); CGAL_CONST(COUNTERCLOCKWISE);
+  CGAL_CONST(COLLINEAR); CGAL_CONST(COPLANAR); CGAL_CONST(DEGENERATE);
+
+  // Oriented_side
+  CGAL_CONST(ON_NEGATIVE_SIDE); CGAL_CONST(ON_ORIENTED_BOUNDARY); CGAL_CONST(ON_POSITIVE_SIDE);
+
+  // Comparison_result
+  CGAL_CONST(SMALLER); CGAL_CONST(EQUAL); CGAL_CONST(LARGER);
 }
 
 #undef CGAL_CONST
-#undef CGAL_ENUM
-#undef CGAL_SENUM
