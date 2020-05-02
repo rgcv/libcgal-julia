b9c73d8d2c75204af4155c6da30846b91003b2ed
Rui Ventura <rventura.pt@outlook.com>
2020-05-03 00:05:47 +0100

    Make `Point_23` wrapping more identical

--- src/kernel/point_2.cpp
+++ src/kernel/point_2.cpp
@@ -15,13 +15,13 @@ void wrap_point_2(jlcxx::Module& kernel, jlcxx::TypeWrapper<Point_2>& point_2) {
     .BINARY_OP_SELF(const Point_2&, <=)
     .BINARY_OP_SELF(const Point_2&, >=)
     .BINARY_OP_SELF(const Point_2&,  -)
-    .BINARY_OP(const Point_2&,      +, const Vector_2&    )
-    .BINARY_OP(const Point_2&,      -, const Vector_2&    )
     .BINARY_OP(const Point_2&,      -, const CGAL::Origin&)
     .BINARY_OP(const CGAL::Origin&, -, const Point_2&     )
-    .BINARY_OP(const Point_2&,     ==, const CGAL::Origin&)
+    .BINARY_OP(const Point_2&,      +, const Vector_2&    )
+    .BINARY_OP(const Point_2&,      -, const Vector_2&    )
     // Operations
     .BINARY_OP_SELF(const Point_2&, ==)
+    .BINARY_OP(const Point_2&, ==, const CGAL::Origin&)
     UNSET_OVERRIDE(kernel, point_2)
     // Creation
     .CTOR(const CGAL::Origin&)
