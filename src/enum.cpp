d887f187eb44f6b4030710ebc776c79cd1b3e117
Rui Ventura <rventura.pt@outlook.com>
2020-07-08 05:51:28 +0100

    Remove CppEnum-derived type methods
    It seems that in the latest version of CxxWrap, since CppEnum derives
    from Integer, methods for subtypes of CppEnum end up creating methods
    for super types as well. This lead to overriding methods like
    `*(::Integer, ::Integer)` or `-(::Integer)`, leading to precompilation
    issues and crashes. See JuliaInterop/libcxxwrap-julia#244.

--- src/enum.cpp
+++ src/enum.cpp
@@ -11,10 +11,10 @@
 void wrap_enum(jlcxx::Module& cgal) {
   CGAL_SENUM(Sign);
   OVERRIDE_BASE(cgal,);
-  cgal.UNARY_OP(-, CGAL::Sign);
-  cgal.BINARY_OP(CGAL::Sign, *, CGAL::Sign);
+  /* cgal.UNARY_OP(-, CGAL::Sign); */
+  /* cgal.BINARY_OP(CGAL::Sign, *, CGAL::Sign); */
   UNSET_OVERRIDE(cgal,);
-  cgal.SPFUNC(CGAL, opposite, CGAL::Sign);
+  /* cgal.SPFUNC(CGAL, opposite, CGAL::Sign); */
   // Sign
   CGAL_CONST(NEGATIVE ); CGAL_CONST(ZERO    ); CGAL_CONST(POSITIVE  );
   CGAL_CONST(COLLINEAR); CGAL_CONST(COPLANAR); CGAL_CONST(DEGENERATE);
@@ -27,11 +27,11 @@ void wrap_enum(jlcxx::Module& cgal) {
   CGAL_CONST(SMALLER); CGAL_CONST(EQUAL); CGAL_CONST(LARGER);
 
   CGAL_ENUM(Bounded_side, BoundedSide);
-  CGAL_UNAMBIG_FUNC(CGAL::Bounded_side, opposite, CGAL::Bounded_side);
+  /* CGAL_UNAMBIG_FUNC(CGAL::Bounded_side, opposite, CGAL::Bounded_side); */
   CGAL_CONST(ON_UNBOUNDED_SIDE); CGAL_CONST(ON_BOUNDARY); CGAL_CONST(ON_BOUNDED_SIDE);
 
   CGAL_SENUM(Angle);
-  CGAL_UNAMBIG_FUNC(CGAL::Angle, opposite, CGAL::Angle);
+  /* CGAL_UNAMBIG_FUNC(CGAL::Angle, opposite, CGAL::Angle); */
   CGAL_CONST(OBTUSE); CGAL_CONST(RIGHT); CGAL_CONST(ACUTE);
 
   CGAL_ENUM(Box_parameter_space_2, BoxParameterSpace2);
