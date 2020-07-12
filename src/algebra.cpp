e5afb65e886e0b90a466e9a3c879089b9616548e
Rui Ventura <rventura.pt@outlook.com>
2020-07-12 10:27:45 +0100

    Define some algebraic methods on NT only if needed

--- src/algebra.cpp
+++ src/algebra.cpp
@@ -10,16 +10,16 @@ typedef Kernel::FT NT;
 void wrap_algebra(jlcxx::Module& cgal) {
   // missing functions that involve EuclideanRing concept:
   // div, div_mod, mod
-  OVERRIDE_BASE(cgal,);
 #ifdef JLCGAL_EXACT_CONSTRUCTIONS
+  OVERRIDE_BASE(cgal,);
   CGAL_GLOBAL_FUNCTION(NT, abs, const NT&);
   CGAL_GLOBAL_FUNCTION(NT, sqrt, const NT&);
   cgal.method("iszero", &CGAL::is_zero<NT>);
   cgal.method("isone", &CGAL::is_one<NT>);
   cgal.method("float", &CGAL::to_double<NT>);
-#endif
   CGAL_GLOBAL_FUNCTION(CGAL::Sign, sign, const NT&);
   UNSET_OVERRIDE(cgal,);
+#endif
   CGAL_GLOBAL_FUNCTION(CGAL::Comparison_result, compare, const NT&, const NT&);
   CGAL_GLOBAL_FUNCTION(NT, integral_division, const NT&, const NT&);
   CGAL_GLOBAL_FUNCTION(NT, inverse, const NT&);
