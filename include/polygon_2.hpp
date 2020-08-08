5b34ec5956664d6ff134f71c0104a2b41e0a97f7
Rui Ventura <rventura.pt@outlook.com>
2020-08-08 17:52:56 +0100

    Pull 2D Polygon typedefs into header

---
+++ include/polygon_2.hpp
@@ -0,0 +1,11 @@
+#include <CGAL/Polygon_2.h>
+#include <CGAL/Polygon_with_holes_2.h>
+
+#include "kernel.hpp"
+
+namespace jlcgal {
+
+typedef CGAL::Polygon_2<Kernel>            Polygon_2;
+typedef CGAL::Polygon_with_holes_2<Kernel> Polygon_with_holes_2;
+
+} // jlcgal
