6f81b91a39c284c0162a51be1e57c45841fda8f3
Benoît Legat <benoit.legat@gmail.com>
2022-08-05 15:45:57 +0200

    Add triangulation_3 in include

--- include/triangulation.hpp
+++ include/triangulation.hpp
@@ -10,6 +10,12 @@
 
 #include <CGAL/Regular_triangulation_2.h>
 
+#include <CGAL/Triangulation_3.h>
+
+#include <CGAL/Delaunay_triangulation_3.h>
+
+#include <CGAL/Regular_triangulation_3.h>
+
 #include "kernel.hpp"
 
 namespace jlcgal {
@@ -22,6 +28,12 @@ typedef CGAL::Constrained_Delaunay_triangulation_2<Kernel> CDTr_2;
 typedef CGAL::Delaunay_triangulation_2<Kernel> DTr_2;
 
 typedef CGAL::Regular_triangulation_2<Kernel> RTr_2;
+
+typedef CGAL::Triangulation_3<Kernel> Tr_3;
+
+typedef CGAL::Delaunay_triangulation_3<Kernel> DTr_3;
+
+typedef CGAL::Regular_triangulation_3<Kernel> RTr_3;
 
 } // jlcgal
 
